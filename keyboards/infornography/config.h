585d7243eaf66d1dc9edd5ded68b87b69edebfc0
ibuky <ibuky>
2025-04-08 20:55:40 +0900

    confirmed functionality

---
+++ keyboards/infornography/config.h
@@ -0,0 +1,11 @@
+// 1000hz Polling Rate
+#define USB_POLLING_INTERVAL_MS 1
+// Bootmagic key position
+// the most left upper key
+#define BOOTMAGIC_ROW 0
+#define BOOTMAGIC_COLUMN 0
+
+#define SPI_SCK_PIN GP18
+#define SPI_MOSI_PIN GP19
+#define SPI_MISO_PIN GP16
+#define PMW33XX_CS_PIN GP17 // SPI CS pin.
